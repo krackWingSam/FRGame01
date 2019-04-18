397a55e1a78b596c32ecb54526fd8e79a59185d9
krackwingsam <dev.fermata@gmail.com>
2019-04-18 09:36:55 +0900

    Binder Library added

---
+++ FRGame01/FRGame01_S/Models/Score.swift
@@ -0,0 +1,14 @@
+//
+//  Score.swift
+//  FRGame01_S
+//
+//  Created by exs-mobile 강상우 on 17/04/2019.
+//  Copyright © 2019 강상우. All rights reserved.
+//
+
+import UIKit
+import EXBinder
+
+class Score: EXModel {
+    
+}
---
+++ FRGame01/FRGame01_S/Statics/FRScore.swift
@@ -0,0 +1,32 @@
+//
+//  FRScore.swift
+//  FRGame01_S
+//
+//  Created by 강상우 on 01/04/2019.
+//  Copyright © 2019 강상우. All rights reserved.
+//
+
+import UIKit
+
+class FRScore: NSObject {
+    static let shared = FRScore()
+    
+    var tileCount: Int = 0
+    var tilePoint: [Int] = []
+    
+    override private init() {
+        super.init()
+        
+        tileCount = FRPreference.shared.tileRange
+        tilePoint = []
+        for i in 0..<tileCount {
+            tilePoint[i] = 0
+        }
+    }
+    
+    
+    public func clearPoint() {
+        tilePoint = []
+    }
+    
+}
